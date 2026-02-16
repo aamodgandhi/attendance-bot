8b468b9ef3d322a55b4160d6cbae36f1dac6c6bb
Aamod Gandhi <55615255+aamodgandhi@users.noreply.github.com>
2026-02-16 14:04:21 -0600

    EVERYTHING WORKS

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ def run():
         page = context.new_page()
 
         try:
-            url = "https://docs.google.com/forms/d/e/1FAIpQLScyoEYQne1Zt273Hrlqx57qUALeL0CrNjn-iL-boUTqzOlFEg/viewform"
+            url = "https://docs.google.com/forms/d/e/1FAIpQLSey1HilfABtgyjfpxSnT28tPBsIDBco9nzG270MAO0AXbblvw/viewform"
             page.goto(url, wait_until="networkidle")
             
             # 1. Selection Logic
